bb9668f1bf0f3ea5689bc1fceca0387c98eda6a3
SazonovDV <34962740+SazonovDV@users.noreply.github.com>
2018-02-19 15:21:27 +0500

    Update cpu-miner.c

--- cpu-miner.c
+++ cpu-miner.c
@@ -3307,8 +3307,7 @@ static int thread_create(struct thr_info *thr, void* func)
 
 static void show_credits()
 {
-	printf("** " PACKAGE_NAME " " PACKAGE_VERSION " by tpruvot@github **\n");
-	printf("BTC donation address: 1FhDPLPpw18X4srecguG3MxJYe4a1JsZnd (tpruvot)\n\n");
+	printf("** " PACKAGE_NAME " " PACKAGE_VERSION " **\n");
 }
 
 void get_defconfig_path(char *out, size_t bufsize, char *argv0);
